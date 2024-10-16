abad75e27ae548a545ee6e6dfb95a99a14ec3572
shongeorg <shongeorg48@gmail.com>
2024-10-16 20:00:41 +0300

    init pg

--- api/index.js
+++ api/index.js
@@ -1,11 +1,54 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import postgres from "postgres";
+import dotenv from "dotenv";
 
-const app = new Hono().basePath('/api')
+dotenv.config();
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+const app = new Hono().basePath("/api");
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+});
+
+app.use("*", (c, next) => {
+  c.res.headers.set("Access-Control-Allow-Origin", "*");
+  c.res.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  c.res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (c.req.method === "OPTIONS") {
+    return c.res.sendStatus(204);
+  }
+
+  return next();
+});
+
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
+
+app.get("/posts", async (c) => {
+  try {
+    const posts = await sql`SELECT * FROM "Post"`;
+    return c.json(posts);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return c.status(500).json({ error: "Internal server error" });
+  }
+});
 
 const handler = handle(app);
 
